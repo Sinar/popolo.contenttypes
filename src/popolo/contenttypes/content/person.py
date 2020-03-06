739ba41ff2462d9b7e0b649fd08940d2936f0815
Khairil Yusof <khairil.yusof@gmail.com>
2020-03-06 15:04:43 +0800

    Add initial Popolo fields for Person Add notes field and make it searchable

--- src/popolo/contenttypes/content/person.py
+++ src/popolo/contenttypes/content/person.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
-# from plone.autoform import directives
+from plone.app.textfield import RichText
+from plone.autoform import directives
 from plone.dexterity.content import Container
-# from plone.namedfile import field as namedfile
+# from plone.namedfile import field as namedimage
+from plone.namedfile import field
 from plone.supermodel import model
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
 
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
 
-# from popolo.contenttypes import _
+from collective import dexteritytextindexer
+
+from popolo.contenttypes import _
 
 
 class IPerson(model.Schema):
@@ -28,10 +33,48 @@ class IPerson(model.Schema):
     #     required=True
     # )
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    title = schema.TextLine(
+        title=_(u'Name'),
+        description=_(u'A person\'s preferred full name'),
+        )
+
+    description = schema.Text(
+        title=_(u'Description'),
+        description=_(u'One line description of this person'),
+        )
+
+
+    gender = schema.Choice(
+        title=_(u'Gender'),
+        vocabulary=SimpleVocabulary([
+            SimpleTerm(value=u'male', title=_(u'Male')),
+            SimpleTerm(value=u'female', title=_(u'Female')),
+            SimpleTerm(value=u'other', title=_(u'Other'))]
+            ),
+        required = False,
+        )
+
+    birth_date = schema.Date(
+        title=_(u'Date of Birth'),
+        required=False,
+        )
+
+    death_date = schema.Date(
+        title=_(u'Date of Death'),
+        required=False,
+        )
+
+    image = field.NamedImage(
+        title=_(u"Headshot"),
+        description=_(u'Image file of a headshot person'),
+        required=False,
+        )
+
+    dexteritytextindexer.searchable('notes')
+    notes = RichText(
+         title=_(u'Notes'),
+         required=False
+     )
 
     # url = schema.URI(
     #     title=_(u'Link'),
