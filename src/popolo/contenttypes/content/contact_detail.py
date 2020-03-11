ad8d90f17d4dde30236528b3098fb990913c62ec
Khairil Yusof <khairil.yusof@gmail.com>
2020-03-11 19:00:55 -0300

    Implement Contact Detail fields as per popolo-spec
    Fixes #7

--- src/popolo/contenttypes/content/contact_detail.py
+++ src/popolo/contenttypes/content/contact_detail.py
@@ -6,11 +6,27 @@ from plone.dexterity.content import Item
 from plone.supermodel import model
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
 from zope.interface import implementer
 
+from collective import dexteritytextindexer
 
-# from popolo.contenttypes import _
+from popolo.contenttypes import _
+
+contact_types = SimpleVocabulary(
+        [
+            SimpleTerm(value=u'cell', title=_(u'Mobile Phone Number')),
+            SimpleTerm(value=u'voice', title=_(u'Voice Phone Number')),
+            SimpleTerm(value=u'text',
+                       title=_(u'A Phone number for text messages')),
+            SimpleTerm(value=u'email', title=_(u'An email address')),
+            SimpleTerm(value=u'address', title=_(u'A postal address')),
+            SimpleTerm(value=u'url', title=_(u'A URL to a contact form')),
+            SimpleTerm(value=u'twitter', title=_(u'Twitter Handle')),
+        ],
+)
 
 
 class IContactDetail(model.Schema):
@@ -21,43 +37,44 @@ class IContactDetail(model.Schema):
 
     # model.load('contact_detail.xml')
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    dexteritytextindexer.searchable('label')
+    label = schema.TextLine(
+        title=_(u'Label'),
+        description=_(u"A human-readable label for the contact detail"),
+        )
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    type = schema.Choice(
+        title=_(u'Type'),
+        description=_(u'A type of medium, eg. mobile phone or email'),
+        vocabulary=contact_types,
+        )
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
+    dexteritytextindexer.searchable('value')
+    value = schema.TextLine(
+        title=_(u'Value'),
+        description=_(u"A value, e.g. a phone number or email address"),
+        )
 
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
+    dexteritytextindexer.searchable('note')
+    note = schema.Text(
+        title=_(u'Note'),
+        description=_(u"A note, e.g. for grouping contact details by " +
+                      u"physical location"),
+        required=False,
+        )
 
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    # valid_from and valid_until will use Plone's effective_date
+    # and expiry_date standard fields
 
 
 @implementer(IContactDetail)
 class ContactDetail(Item):
     """
     """
+
+    def Title(self):
+        return self.label
+
+    @property
+    def title(self):
+        return self.label
