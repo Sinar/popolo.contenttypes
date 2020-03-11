058f3e7ab8272bc7bde0172e1c2345d75c89bce8
Khairil Yusof <khairil.yusof@gmail.com>
2020-03-11 06:58:02 -0300

    Revert ""Add content_type: Source""
    This reverts commit b6c8bef4bfef5b34d6a7d4aee4bce66f080a388e.

--- src/popolo/contenttypes/content/contact_detail.py
+++ src/popolo/contenttypes/content/contact_detail.py
@@ -1,28 +1,17 @@
 # -*- coding: utf-8 -*-
-from plone.app.textfield import RichText
+# from plone.app.textfield import RichText
 # from plone.autoform import directives
 from plone.dexterity.content import Item
 # from plone.namedfile import field as namedfile
 from plone.supermodel import model
-from plone.dexterity.content import Container
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-from zope import schema
+# from zope import schema
 from zope.interface import implementer
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
-from popolo.contenttypes import _
 
-contact_types = SimpleVocabulary([
-            SimpleTerm(value=u'cell', title=_(u'Mobile Phone Number')),
-            SimpleTerm(value=u'voice', title=_(u'Voice Phone Number')),
-            SimpleTerm(value=u'email', title=_(u'Email Address')),
-            SimpleTerm(value=u'address', title=_(u'Full street address')),
-            SimpleTerm(value=u'twitter', title=_(u'Twitter Handle')),
-            SimpleTerm(value=u'facebook', 
-                title=_(u'Facebook URL')),
-            ]
-            )
+# from popolo.contenttypes import _
+
 
 class IContactDetail(model.Schema):
     """ Marker interface and Dexterity Python Schema for ContactDetail
@@ -32,33 +21,7 @@ class IContactDetail(model.Schema):
 
     # model.load('contact_detail.xml')
 
-    title = schema.TextLine(
-        title=_(u'Label'),
-        description=_(u"A human-readable label for the contact detail"),
-        )
-
-    type = schema.Choice(
-        title=_(u'Type of Medium'),
-        vocabulary=contact_types,
-        required=True,
-        )
-
-    value = schema.TextLine(
-            title=_(u'Value'),
-            description=_(u'eg. +60 323 4344 for mobile,' +
-                          ' myemailaddress.com for email')
-            )
-
-    note = schema.Text(
-            title=_(u'Note'),
-            description=_(u'A note, e.g. for grouping contact details by ' +
-                          'physical location',),
-                    )
-
-    # TODO Add start_date and end_date for valid_from and valid_until
-    # fields
-
-    # directivesmil.widget(level=RadioFieldWidget)
+    # directives.widget(level=RadioFieldWidget)
     # level = schema.Choice(
     #     title=_(u'Sponsoring Level'),
     #     vocabulary=LevelVocabulary,
@@ -93,7 +56,8 @@ class IContactDetail(model.Schema):
     #     required=False
     # )
 
+
 @implementer(IContactDetail)
-class ContactDetail(Container):
+class ContactDetail(Item):
     """
     """
