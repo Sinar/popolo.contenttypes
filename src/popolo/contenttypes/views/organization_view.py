1eb0e9bb8c48c78640b28ea8da78e4af19a3d1b5
Khairil Yusof <khairil.yusof@gmail.com>
2020-03-17 17:57:00 +0800

    Initial Custom View for Organization
    With Parent Organization Relations

--- src/popolo/contenttypes/views/organization_view.py
+++ src/popolo/contenttypes/views/organization_view.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 
+from plone import api
 from popolo.contenttypes import _
 from Products.Five.browser import BrowserView
+from plone.dexterity.browser.view import DefaultView
 
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
-class OrganizationView(BrowserView):
+class OrganizationView(DefaultView,BrowserView):
     # If you want to define a template here, please remove the template from
     # the configure.zcml registration of this view.
     # template = ViewPageTemplateFile('organization_view.pt')
 
-    def __call__(self):
-        # Implement your own actions:
-        self.msg = _(u'A small message')
-        return self.index()
+    def boo(self):
+        text = u'Boo'
+        return text
