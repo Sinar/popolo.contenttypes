eb6b63ceb846bf88f30d7c406abe160704cfc134
Khairil Yusof <khairil.yusof@gmail.com>
2020-03-18 16:21:40 +0800

    Initial Popolo-spec Membership Class
    * Fields as per popolo-spec for Membership Class
    * Allow Contact_Detail, Link, File as sub-types
    * Allow Organization type to add Memberships

--- src/popolo/contenttypes/content/membership.py
+++ src/popolo/contenttypes/content/membership.py
@@ -6,11 +6,18 @@ from plone.dexterity.content import Container
 from plone.supermodel import model
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
+from collective import dexteritytextindexer
+from z3c.relationfield.schema import RelationChoice
+
+from plone.app.vocabularies.catalog import CatalogSource
+
+from plone.app.z3cform.widget import RelatedItemsFieldWidget
+from plone.autoform import directives
+
 from zope.interface import implementer
 
-
-# from popolo.contenttypes import _
+from popolo.contenttypes import _
 
 
 class IMembership(model.Schema):
@@ -21,43 +28,122 @@ class IMembership(model.Schema):
 
     # model.load('membership.xml')
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    dexteritytextindexer.searchable('label')
+    label = schema.TextLine(
+        title=_(u'Label'),
+        description=_(u"A label describing the membership, " +
+                      "eg. Member of Parliament of Kericho County"),
+        required=True,)
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    dexteritytextindexer.searchable('role')
+    role = schema.TextLine(
+        title=_(u'Role'),
+        description=_(u"A role that the member fulfills in, " +
+                      "in the organization. eg. " +
+                      "Member of Parliament"),
+        required=True,)
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
+    # FIXME Persons and Organizations should be Required but unable to
+    # select on creation due to upstream bug:
+    # https://github.com/plone/plone.app.vocabularies/issues/59
 
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
+    # Temporary workaround, save first, then edit to select person and
+    # organization for membership
 
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
+    # Person
+    directives.widget('person',
+                      RelatedItemsFieldWidget,
+                      pattern_options={
+                        'basePath': '/',
+                        'mode': 'auto',
+                        'favourites': [],
+                        }
+                      )
 
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    person = RelationChoice(
+            title=u'Person',
+            source=CatalogSource(portal_type='Person'),
+            required=False,
+            )
 
+    # Organization
+    directives.widget('organization',
+                      RelatedItemsFieldWidget,
+                      pattern_options={
+                        'basePath': '/',
+                        'mode': 'auto',
+                        'favourites': [],
+                        }
+                      )
+    organization = RelationChoice(
+            title=u'Organization',
+            source=CatalogSource(portal_type='Organization'),
+            required=False,
+            )
+
+    # Post
+    directives.widget('post',
+                      RelatedItemsFieldWidget,
+                      pattern_options={
+                        'mode': 'auto',
+                        'favourites': [],
+                        }
+                      )
+
+    post = RelationChoice(
+            title=u'Post',
+            source=CatalogSource(portal_type='Post'),
+            required=False,
+            )
+
+    # On Behalf Of
+    directives.widget('on_behalf_of',
+                      RelatedItemsFieldWidget,
+                      pattern_options={
+                        'basePath': '/',
+                        'mode': 'auto',
+                        'favourites': [],
+                        }
+                      )
+    on_behalf_of = RelationChoice(
+            title=_(u'On Behalf Of'),
+            description=_(u'The organization on whose behalf the ' +
+                          'person is a member of the organization'),
+            source=CatalogSource(portal_type='Organization'),
+            required=False,
+            )
+
+    start_date = schema.Date(
+        title=_(u'Start Date'),
+        description=_(u'Date which this relationship began'),
+        required=False,)
+
+    end_date = schema.Date(
+        title=_('End Date'),
+        description=_(u'Date which this relationship ended'),
+        required=False,)
 
 @implementer(IMembership)
 class Membership(Container):
     """
     """
+
+    @property
+    def title(self):
+        ''' return label'''
+        return self.label
+
+    @title.setter
+    def title(self, value):
+        ''' we wont set a title here'''
+        pass
+
+    @property
+    def description(self):
+        ''' return role'''
+        return self.role
+
+    @description.setter
+    def description(self, value):
+        ''' we wont set a title here'''
+        pass
