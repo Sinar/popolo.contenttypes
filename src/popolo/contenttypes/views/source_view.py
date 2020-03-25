c42de13d9703ce2e8a521947c9ddcaa5dd3b875f
Khairil Yusof <khairil.yusof@gmail.com>
2020-03-25 17:50:04 +0800

    "Add view: source-view"

---
+++ src/popolo/contenttypes/views/source_view.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from popolo.contenttypes import _
+from plone.dexterity.browser.view import DefaultView
+
+# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class SourceView(DefaultView):
+    # If you want to define a template here, please remove the template from
+    # the configure.zcml registration of this view.
+    # template = ViewPageTemplateFile('source_view.pt')
+
+    def __call__(self):
+        # Implement your own actions:
+        return super(SourceView, self).__call__()
