fdc92576cb50ad31f5051f0ea5934a9f69fda0fc
Khairil Yusof <khairil.yusof@gmail.com>
2020-04-19 21:41:58 +0800

    Initial fields for Relathionship Class
    Addable under Person and Organization types

--- src/popolo/contenttypes/content/relationship.py
+++ src/popolo/contenttypes/content/relationship.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
+from plone.app.textfield import RichText
 # from plone.autoform import directives
 from plone.dexterity.content import Container
 # from plone.namedfile import field as namedfile
 from plone.supermodel import model
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
+from collective import dexteritytextindexer
+from z3c.relationfield.schema import RelationChoice
+
+from plone.app.vocabularies.catalog import CatalogSource
+
+from plone.app.z3cform.widget import RelatedItemsFieldWidget
+from plone.autoform import directives
+
 from zope.interface import implementer
 
-
-# from popolo.contenttypes import _
+from popolo.contenttypes import _
 
 
 class IRelationship(model.Schema):
@@ -21,43 +28,90 @@ class IRelationship(model.Schema):
 
     # model.load('relationship.xml')
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    dexteritytextindexer.searchable('name')
+    name = schema.TextLine(
+        title=_(u'Relationship Name'),
+        description=_(u'''
+        The name of this relationship, eg. Son of Person,
+        Known business associate of Person, Major client of Business.
+                       '''),
+        required=True,
+        )
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    dexteritytextindexer.searchable('description')
+    description = schema.Text(
+        title=_(u'Description'),
+        description=_(u'Brief description of this relationship'),
+        required=False,
+        )
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
+    # Subject
+    directives.widget('subject',
+                      RelatedItemsFieldWidget,
+                      pattern_options={
+                        'basePath': '/',
+                        'mode': 'auto',
+                        'favourites': [],
+                        }
+                      )
 
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
+    subject = RelationChoice(
+            title=u'Subject',
+            description=_(u'''
+                The subject of the relation.
+                            '''),
+            source=CatalogSource(portal_type=['Person', 'Organization']),
+            required=False,
+            )
 
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
+    # Object
+    directives.widget('object',
+                      RelatedItemsFieldWidget,
+                      pattern_options={
+                        'basePath': '/',
+                        'mode': 'auto',
+                        'favourites': [],
+                        }
+                      )
 
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    object = RelationChoice(
+            title=u'Object',
+            description=_(u'''
+                The object of the relation.
+                            '''),
+            source=CatalogSource(portal_type=['Person', 'Organization']),
+            required=False,
+            )
+
+    start_date = schema.Date(
+        title=_(u'Start Date'),
+        description=_(u'Date which this relationship began'),
+        required=False,)
+
+    end_date = schema.Date(
+        title=_('End Date'),
+        description=_(u'Date which this relationship ended'),
+        required=False,)
+
+    # Public notes
+    dexteritytextindexer.searchable('notes')
+    notes = RichText(
+         title=_(u'Notes'),
+         required=False
+     )
 
 
 @implementer(IRelationship)
 class Relationship(Container):
     """
     """
+
+    @property
+    def title(self):
+        ''' return name'''
+        return self.name
+
+    @title.setter
+    def title(self, value):
+        ''' we wont set a title here'''
+        pass
