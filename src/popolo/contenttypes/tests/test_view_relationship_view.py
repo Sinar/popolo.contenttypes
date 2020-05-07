1083001869bfd71345719e7579642804e3a0e48c
Khairil Yusof <khairil.yusof@gmail.com>
2020-05-07 10:30:32 +0800

    "Add view: relationship-view"

---
+++ src/popolo/contenttypes/tests/test_view_relationship_view.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from popolo.contenttypes.testing import POPOLO_CONTENTTYPES_FUNCTIONAL_TESTING
+from popolo.contenttypes.testing import POPOLO_CONTENTTYPES_INTEGRATION_TESTING
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from zope.component import getMultiAdapter
+from zope.component.interfaces import ComponentLookupError
+
+import unittest
+
+
+class ViewsIntegrationTest(unittest.TestCase):
+
+    layer = POPOLO_CONTENTTYPES_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        api.content.create(self.portal, 'Folder', 'other-folder')
+        api.content.create(self.portal, 'Document', 'front-page')
+
+    def test_relationship_view_is_registered(self):
+        view = getMultiAdapter(
+            (self.portal['other-folder'], self.portal.REQUEST),
+            name='relationship-view'
+        )
+        self.assertTrue(view.__name__ == 'relationship-view')
+        # self.assertTrue(
+        #     'Sample View' in view(),
+        #     'Sample View is not found in relationship-view'
+        # )
+
+    def test_relationship_view_not_matching_interface(self):
+        with self.assertRaises(ComponentLookupError):
+            getMultiAdapter(
+                (self.portal['front-page'], self.portal.REQUEST),
+                name='relationship-view'
+            )
+
+
+class ViewsFunctionalTest(unittest.TestCase):
+
+    layer = POPOLO_CONTENTTYPES_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
