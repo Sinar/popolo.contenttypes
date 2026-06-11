41b4d6012651e23f1b249f336286ff9e59ae194e
Khairil Yusof <khairil.yusof@gmail.com>
2026-06-11 14:48:37 +0800

    Upgrade to Plone 6.2
    - setup.py: Framework :: Plone :: 5.2 -> 6.2
    - setup.py: plone.testing>=5.0.0 -> >=8.0.0
    - buildout.cfg: test_plone52.cfg -> test_plone62.cfg
    - Added test_plone62.cfg

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: Addon",
-        "Framework :: Plone :: 5.2",
+        "Framework :: Plone :: 6.2",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
@@ -46,7 +46,7 @@ setup(
     package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
-    python_requires="==2.7, >=3.6",
+    python_requires=">=3.12",
     install_requires=[
         'setuptools',
         # -*- Extra requirements: -*-
@@ -61,7 +61,7 @@ setup(
             # Plone KGS does not use this version, because it would break
             # Remove if your package shall be part of coredev.
             # plone_coredev tests as of 2016-04-01.
-            'plone.testing>=5.0.0',
+            'plone.testing>=8.0.0',
             'plone.app.contenttypes',
             'plone.app.robotframework[debug]',
         ],
